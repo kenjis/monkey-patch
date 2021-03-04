302af19762b2efb90c11f092fe5e33661afcd00a
kenjis <kenji.uui@gmail.com>
2021-03-04 13:59:38 +0900

    chore: add bootstrap for phpstan

---
+++ phpstan_constants.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Copyright (c) 2021 Kenji Suzuki
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/kenjis/monkey-patch
+ */
+
+// See https://phpstan.org/user-guide/discovering-symbols#global-constants
+
+define('CONST1', 'CONST1 value');
+define('CONST2', 'CONST2 value');
