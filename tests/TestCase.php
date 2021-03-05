12de2d4ea6c530e13bad3fe2ebb7dbaed27ccb15
kenjis <kenji.uui@gmail.com>
2021-03-05 13:11:42 +0900

    chore: add `use DebugHelper`

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Kenjis\MonkeyPatch;
 
+use Kenjis\PhpUnitHelper\DebugHelper;
+
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
+    use DebugHelper;
 }
